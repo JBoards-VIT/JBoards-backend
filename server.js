7357abf36632d6275bdb31c3ecf3a3e1511fa9c7
AryakRoy <aryakroy63@gmail.com>
2021-12-03 10:05:10 +0530

    Added routes to server file and updated gitignore

--- server.js
+++ server.js
@@ -15,7 +15,10 @@ app.get('/', (req, res) => res.send('API Running!!'))
 
 app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/project', require('./routes/api/project'))
+app.use('/api/kanban', require('./routes/api/kanban'))
 
-const PORT = process.env.PORT || 4999
+
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`))
